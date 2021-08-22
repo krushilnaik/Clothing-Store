9174e84beae65e0ab731825f36ecaf02737753ce
Krushil Naik <krushilnaik96@gmail.com>
2021-08-22 14:49:59 -0400

    Add custom large menu item, switch back to SCSS

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -6,18 +6,17 @@ interface Props {
 	// size: string;
 	id: number;
 	linkUrl: string;
+	large?: boolean;
 }
 
-function MenuItem({ title, imageUrl, id, linkUrl }: Props): ReactElement {
+function MenuItem({ title, imageUrl, id, linkUrl, large = false }: Props): ReactElement {
 	return (
 		<div
 			style={{ backgroundImage: `url(${imageUrl})` }}
-			className='min-w-[30%] h-60 flex-auto flex items-center justify-center border-2 border-black m-2 first:mr-2 last:ml-2 bg-center bg-cover'
+			className={`menu-item ${large && 'large'}`}
 		>
-			<div className='h-24 px-6 flex flex-col items-center justify-center border-2 border-black'>
-				<h1 className='font-bold mb-2 text-xl text-gray-500 uppercase'>
-					{title}
-				</h1>
+			<div>
+				<h1 className='font-bold mb-2 text-2xl text-gray-600 uppercase'>{title}</h1>
 				<span className='font-light text-base'>SHOP NOW</span>
 			</div>
 		</div>
