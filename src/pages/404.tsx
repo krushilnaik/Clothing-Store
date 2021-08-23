acad8b8100112a45807cd2479db9946fe29f9043
Krushil Naik <krushilnaik96@gmail.com>
2021-08-23 10:25:37 -0400

    Plan for custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+interface Props {}
+
+function NotFoundPage({}: Props): ReactElement {
+	return <main>Page not found</main>;
+}
+
+export default NotFoundPage;
