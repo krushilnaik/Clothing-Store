ca1e15e38fef19d3da3670e8158d2e995108a9e2
Krushil Naik <krushilnaik96@gmail.com>
2021-08-26 13:49:28 -0400

    Move sign in page to /sign-in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,14 @@ interface Props {}
 
 function Header({}: Props): ReactElement {
 	return (
-		<header className='flex flex-row items-center justify-between'>
+		<header className='flex flex-row items-center justify-between px-2'>
 			<Link href='/'>LOGO</Link>
 
-			<ul className='flex flex-row justify-between gap-12'>
+			<ul className='flex flex-row justify-between gap-16 text-lg'>
 				<NavLink link='/shop' text='Shop' />
 				<NavLink link='/contact' text='Contact' />
-				<NavLink link='/signIn' text='Sign In' />
+				<NavLink link='/sign-in' text='Sign In' />
+
 				<button>Cart</button>
 			</ul>
 		</header>
